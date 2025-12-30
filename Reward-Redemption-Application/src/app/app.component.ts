69d2228f66fb329f1731d9e66e2d3a46faf0a70c
Vijay-star1 <vijaykumar.anigeni@tcs.com>
2025-12-30 06:51:45 +0000

    adding frontend code

--- Reward-Redemption-Application/src/app/app.component.ts
+++ Reward-Redemption-Application/src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MatButtonModule, MatTableModule],
+  template: '<router-outlet></router-outlet>',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
